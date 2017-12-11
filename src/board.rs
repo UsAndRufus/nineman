3c23621e0b550122018de74536d99c0ffb4611d7
Edward Bennigsen <edward.bennigsen@protonmail.com>
2017-12-11 02:15:42 +0000

    Board being constructed using new method. Not going back and connecting to previous tiers yet, and corners not being connected

--- src/board.rs
+++ src/board.rs
@@ -1,50 +1,96 @@
+use std::fmt;
+
 use player::Player;
 
+// Idea for a list of indices borrowed from here: https://rust-leipzig.github.io/architecture/2016/12/20/idiomatic-trees-in-rust/
+
 #[derive(Debug)]
-pub struct Position<'a> {
+pub struct Position {
+    // NB not using proper notation as it's a faff to work out with the way I'm generating the board
+    pub id:    String,
     pub piece: Option<Player>,
-    pub north: Option<&'a Position<'a>>,
-    pub south: Option<&'a Position<'a>>,
-    pub east:  Option<&'a Position<'a>>,
-    pub west:  Option<&'a Position<'a>>,
+    pub north: Option<usize>,
+    pub east:  Option<usize>,
+    pub south: Option<usize>,
+    pub west:  Option<usize>,
 }
 
-impl<'a> Position<'a> {
-    pub fn blank() -> Position<'a> {
-        return Position{ piece: None, north: None, south: None, east: None, west: None };
+impl Position {
+    pub fn blank(id: String) -> Position {
+        return Position{ id: id, piece: None, north: None, south: None, east: None, west: None };
     }
 }
 
-#[derive(Debug)]
-pub struct Board<'a> {
-    pub positions: Vec<Position<'a>>,
+pub struct Board {
+    pub positions: Vec<Position>,
     pub players: Vec<Player>,
 }
 
-impl<'a> Board<'a> {
-    pub fn new(player1: Player, player2: Player) -> Board<'a> {
+impl Board {
+    pub fn new(player1: Player, player2: Player) -> Board {
 
         let mut board = Board {
             positions: Vec::new(),
             players: vec![player1, player2],
         };
 
-        let (mut north, mut south, mut east, mut west)
-            = (Position::blank(), Position::blank(), Position::blank(), Position::blank());
-        for _layer in 0..3 {
-            let nw = Position::blank();
-            let ne = Position::blank();
-            let sw = Position::blank();
-            let se = Position::blank();
+        let (mut prev_north, mut prev_south, mut prev_east, mut prev_west) = (None, None, None, None);
+        for layer in 0..3 {
 
-            let north_other = Position { piece: None, north: Some(&north), south: None, east: Some(&ne), west: Some(&sw) };
+            let nw = board.new_blank_position(format!("{}nw", layer));
+            let ne = board.new_blank_position(format!("{}ne", layer));
+            let sw = board.new_blank_position(format!("{}sw", layer));
+            let se = board.new_blank_position(format!("{}se", layer));
 
-            board.positions.push(nw);
-            board.positions.push(ne);
-            board.positions.push(sw);
-            board.positions.push(se);
+            let north = Position { id: format!("{}n", layer), piece: None, north: prev_north, south: None, east: Some(nw), west: Some(sw) };
+            let east  = Position { id: format!("{}e", layer), piece: None, north: Some(ne), south: Some(se), east: prev_east, west: None };
+            let south = Position { id: format!("{}s", layer), piece: None, north: None, south: prev_south, east: Some(sw), west: Some(se) };
+            let west  = Position { id: format!("{}w", layer), piece: None, north: Some(nw), south: Some(sw), east: None, west: prev_west };
+
+            prev_north = Some(board.add_position(north));
+            prev_east = Some(board.add_position(east));
+            prev_south = Some(board.add_position(south));
+            prev_west = Some(board.add_position(west));
         }
 
         return board;
+    }
+
+    fn add_position(&mut self, position: Position) -> usize {
+        let next_index = self.positions.len();
+
+        self.positions.push(position);
+
+        return next_index;
+    }
+
+    fn new_blank_position(&mut self, id: String) -> usize {
+        return self.add_position(Position::blank(id));
+    }
+
+    pub fn get_id(&self, position: Option<usize>) -> String {
+        match position {
+            Some(p) => self.positions[p].id.to_owned(),
+            None => "_".to_string(),
+        }
+    }
+}
+
+impl fmt::Debug for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+        let mut debug_string = String::new();
+
+        for position in &self.positions {
+            debug_string += &format!("({} -> {},{},{},{}), ",
+                position.id,
+                self.get_id(position.north),
+                self.get_id(position.east),
+                self.get_id(position.south),
+                self.get_id(position.west));
+        }
+
+        return write!(f, "{}", debug_string);
+
     }
 }
