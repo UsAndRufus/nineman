0cbbba9dd2203689d79766d7e9afeb68dbc1d2f3
Edward Bennigsen <edward.bennigsen@protonmail.com>
2018-01-02 21:22:01 +0000

    Added iterator for Direction

--- src/board/direction.rs
+++ src/board/direction.rs
@@ -1,7 +1,18 @@
+use std::slice::Iter;
+
+use self::Direction::*;
+
 #[derive(Debug)]
 pub enum Direction {
     North,
     East,
     South,
     West,
+}
+
+impl Direction {
+    pub fn iterator() -> Iter<'static, Direction> {
+        static DIRECTIONS: [Direction; 4] = [North, East, South, West];
+        DIRECTIONS.into_iter()
+    }
 }
